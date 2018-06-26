30ca1d69c37c5e2e866f27decc0ceeed7a385b59
Jose R Orellana Jr <yoborellana_2004@yahoo.com>
2018-06-26 18:12:00 -0500

    still working

--- src/app/components/profile/profile.jsx
+++ src/app/components/profile/profile.jsx
@@ -13,7 +13,7 @@ class Profile extends React.Component {
         return (
             <div>
                 <Header />
-                <h1>{this.state.temp}</h1>
+                <h1>Profile</h1>
 		{/*<h1>{this.props.test}</h1>*/}
             </div>
         )
