6d5c5b783ad27551c98c69843005f28bc396d092
Jose R Orellana Jr <yoborellana_2004@yahoo.com>
2018-09-03 12:59:47 -0500

    able to see profile

--- app.js
+++ app.js
@@ -94,7 +94,9 @@ function requireLogin (req, res, next) {
   }
 }
 
-
+//temporary line below
+app.use('/profile', profile)
+//end of temporary line
 app.use('/friends', friends)
 app.use('/users', users);
 app.use('/signup', signUp )
--- src/app/components/profile/profile.jsx
+++ src/app/components/profile/profile.jsx
@@ -1,12 +1,27 @@
 import React from 'react'
 import Header from '../base/header'
-// import SearchResults from '../search/searchResults'; 
+import axios from 'axios';
 
 class Profile extends React.Component {
 
 	constructor(props) {
 		super(props)
-			this.state =  { parameter: ''};
+			this.state =  { parameter: '',
+			firstName: '',
+			lastName: ''
+			};
+	}
+
+	componentDidMount() {
+		var that = this;
+		axios.get('http://localhost:3000/auth/profile/'+this.props.profileId)
+			.then(function(res) {
+				that.setState({firstName: res.data.firstName}) 
+				that.setState({lastName: res.data.lastName})
+			})
+			.catch(function(error) {
+				console.log(error);
+			});
 	}
 
 
@@ -15,8 +30,12 @@ class Profile extends React.Component {
         return (
             <div>
                 <Header />
-                <h1>Profile</h1>
+                <h1>{this.state.firstName} {this.state.lastName}</h1>
                 <h1>{this.props.profileId}</h1>
+
+		
+
+
 		{/* <SearchResults onSelectPerson={this.onSelectPerson}/> */}
             </div>
         )
